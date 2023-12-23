8848fc6e41896278cf0d967d459422a81fa3eb36
Mihas <mihasK@users.noreply.github.com>
2023-12-24 01:38:38 +0200

    update

--- Makefile
+++ Makefile
@@ -1,2 +1,16 @@
 track:
 	sudo nice -n 10 powermetrics --samplers cpu_power,gpu_power,thermal -o powermetrics.txt -f plist -i 1000
+
+cpu:
+	papermill try_phi2_torch.ipynb output.ipynb --log-output -p MAX_TOKENS 200 -p USE_MPS False
+
+mps:
+	papermill try_phi2_torch.ipynb output.ipynb --log-output -p MAX_TOKENS 200 -p USE_MPS True
+
+mlx:
+	papermill try_phi2_mlx.ipynb output.ipynb --log-output -p MAX_TOKENS 200
+
+# jrun:
+# 	papermill $(s) output.ipynb --log-output
+# jrun: 
+# 	jupyter nbconvert --to script "$(s)" --output _notebook_to_run; cat _notebook_to_run.py | grep -v get_ipython > run_this.py; python3 run_this.py
--- utils.py
+++ utils.py
@@ -3,12 +3,11 @@ import torch
 import psutil
 from tabulate import tabulate
 
-
 # Function to clear memory cache
 def empty_cache():
     gc.collect()
     torch.mps.empty_cache()
-    print('Python (GC) and MPS cache emptied')
+    print('Cache emptied: python (GC) and MPS ')
 
 def mps_memory_usage(to_print=True):
     tensors = round(torch.mps.current_allocated_memory() / 10**6)
@@ -26,12 +25,17 @@ def mps_memory_usage(to_print=True):
 
 def process_memory_usage(to_print=True):
     process = psutil.Process()
-    mem = process.memory_info().rss // 2**20 # in MB
+    process_mem = process.memory_info().rss // 2**20 # in MB
+
+    ram_info = psutil.virtual_memory()
     
     if to_print:
-        print(f"Process memory usage: {mem} MB")  
+        print(f"Process memory usage: {process_mem} MB")  
     
-    return mem
+        ram_info = psutil.virtual_memory()
+
+    return process_mem, (ram_info.total - ram_info.available) // 2**20
+
     
 
 # import time
@@ -49,29 +53,28 @@ class MPS_MemoryTracker(object):
         if self.clean_cache_before:
             empty_cache()
         self.mps_mem_start = mps_memory_usage(to_print=False)
-        self.process_mem_start = process_memory_usage(to_print=False)
+        self.mem_start = process_memory_usage(to_print=False)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         if self.clean_cache_after:
             empty_cache()
         self.mps_mem_end = mps_memory_usage(to_print=False)
-        self.process_mem_end = process_memory_usage(to_print=False)
+        self.mem_end = process_memory_usage(to_print=False)
         
         table = tabulate([
-                ('Before', self.mps_mem_start[0], self.mps_mem_start[1], self.process_mem_start, ),
-                ('After', self.mps_mem_end[0], self.mps_mem_end[1], self.process_mem_end, ),
+                ('Before', self.mps_mem_start[0], self.mps_mem_start[1], self.mem_start[0],self.mem_start[1], ),
+                ('After', self.mps_mem_end[0], self.mps_mem_end[1], self.mem_end[0], self.mem_end[1], ),
                 
                 ('Diff', 
                 '{0:+} MB'.format(self.mps_mem_end[0] - self.mps_mem_start[0]),
                 '{0:+} MB'.format(self.mps_mem_end[1] - self.mps_mem_start[1]),
-                '{0:+} MB'.format(self.process_mem_end - self.process_mem_start),
+                '{0:+} MB'.format(self.mem_end[0] - self.mem_start[0]),
+                '{0:+} MB'.format(self.mem_end[1] - self.mem_start[1]),
                 )
             ],
-            headers=['MPS tensors', 'MPS Total', 'Process Memory']
+            headers=['MPS tensors', 'MPS Total', 'Process Memory', 'Total System Memory']
         )
-        print('######## Memory consumption:')
-        
+        print('######## Memory consumption ########:')
         print(table)
-        # print('MPS Memory increase: {0:+}'.format(self.mps_mem_end[1] - self.mps_mem_start[1]), 'MB')
-        # print('Process memory increase: {0:+}'.format(self.process_mem_end - self.process_mem_start), 'MB')
+        
